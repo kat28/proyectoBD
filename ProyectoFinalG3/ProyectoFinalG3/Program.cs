b1f99ffd3596509faaa5d98be4f6a7625c694093
kat28 <kategodinezd@gmail.com>
2016-10-19 14:24:02 -0600

    .

--- ProyectoFinalG3/ProyectoFinalG3/Program.cs
+++ ProyectoFinalG3/ProyectoFinalG3/Program.cs
@@ -16,7 +16,7 @@ namespace ProyectoFinalG3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MDIPri());
+            Application.Run(new frmInt());
         }
     }
 }
