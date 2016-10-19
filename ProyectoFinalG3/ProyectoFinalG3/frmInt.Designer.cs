6eea8dd4f929066c2beca8405eee8cc7a42ffc05
kat28 <kategodinezd@gmail.com>
2016-10-19 14:22:57 -0600

    etc

---
+++ ProyectoFinalG3/ProyectoFinalG3/frmInt.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace prjBL
+{
+    partial class frmInt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tv = new System.Windows.Forms.TreeView();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 51);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 333);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tv
+            // 
+            this.tv.Location = new System.Drawing.Point(12, 51);
+            this.tv.Name = "tv";
+            this.tv.Size = new System.Drawing.Size(151, 333);
+            this.tv.TabIndex = 1;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(50, 390);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 2;
+            this.btnCargar.Text = "button1";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // frmInt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 442);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.tv);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmInt";
+            this.Text = "frmInt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TreeView tv;
+        private System.Windows.Forms.Button btnCargar;
+    }
+}
---
+++ ProyectoFinalG3/ProyectoFinalG3/frmInt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjBL
+{
+    public partial class frmInt : Form
+    {
+
+        prjAccesoBD.clsConexion FuncionConexion = new prjAccesoBD.clsConexion();
+        prjBL.clsMetodos FuncionLogica = new prjBL.clsMetodos();
+        public string strSentencia;
+
+        public frmInt()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            strSentencia = "select name from sysobjects where type='U'";
+            FuncionLogica.cargaVistaArbol(tv, strSentencia);
+
+
+        }
+    }
+}
